217e726cefd9cc554ee1f0d2675ec8721e75c5de
ling <ziweifengyun@gmail.com>
2025-01-13 20:14:18 +0800

    最新提交，桌面1

--- apps/web/src/utils/sendMessage.js
+++ apps/web/src/utils/sendMessage.js
@@ -2,9 +2,9 @@ import {Channel, Mention, MessageText, WKSDK} from "wukongimjssdk";
 
 //sendMessageToContact('39e096cb77e14983972c33c063509a2c',2,['0f3f6f2f54da4d6fa7a1d6cd065cb10c'],'222222');
 export function sendMessageToContact(channelID, channelType, uids, message){
-    console.log(`-----------------------------------`)
-    console.log(`频道${channelID},lx:${channelType},向联系人:[${uids}],发送消息:[${message}]`)
-    console.log("---sendMessageToContact----");
+    console.log(`----sendMessageToContact----`)
+    console.log(`频道:[${channelID}],lx:[${channelType}],向联系人:[${uids}],发送消息:[${message}]`)
+    console.log('----参数准备')
     //发送消息2, 回复消息会在别人发送之前，顺序有问题
     const c = new Channel(channelID, channelType)
     const content = new MessageText(message)
@@ -15,4 +15,5 @@ export function sendMessageToContact(channelID, channelType, uids, message){
     content.mention = mn
     //this.conversationContext.sendMessage(content, c);
     WKSDK.shared().chatManager.send(content, c)
+    console.log('----发送完成')
 }
